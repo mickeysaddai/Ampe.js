b0e266dd25e38ce93276a48fe62a455d7a65f978
Michelle Addai <88467155+mickeysaddai@users.noreply.github.com>
2021-10-13 22:02:38 -0400

    github and linkedIn

--- src/index.js
+++ src/index.js
@@ -2,19 +2,10 @@ const Game = require('./scripts/game.js');
 
 document.addEventListener("DOMContentLoaded", () => {
     const canvas = document.getElementById('canvas'); //canvas setup
-    const ctx = canvas.getContext('2d'); //allow us to use built in canvas methods
+    ; //allow us to use built in canvas methods
     canvas.width = 500;
     canvas.height = 200;
     console.log('Ampe!!!')
-
-    // const wrapper = document.querySelector('.wrapper');
-    // const grid = document.querySelector('.gridContainer')
-
-    // function addGrid() {
-    //     wrapper.innerHTML += grid.innerHTML;
-    // }
-
-    // addGrid();
 
 
 
@@ -22,128 +13,76 @@ document.addEventListener("DOMContentLoaded", () => {
         document.getElementById("myDropdown").classList.toggle("show");
     })
 
-  
-
-    document.getElementById('start-button').addEventListener('click', function(){
-      console.log('clicked')
-        const numberOfPlayers = prompt('Please enter number between 2 - 8')
-        const game = new Game(ctx, numberOfPlayers);
-        game.drawGame();
-        // console.log('You entered ', numberOfPlayers)
-        // setUpGameBoard(numberOfPlayers)
-        disabled = document.getElementById('start-button').disabled = true;
 
 
+    document.getElementById('start-button').addEventListener('click', initGame)
 
-
-        // const chooseButton = document.createElement('button');
-        // chooseButton.innerText = 'Choose Color';
-
-        document.getElementsByClassName('dropdown')[0].style.visibility = 'visible';
-        // document.getElementById('dropdown').append(chooseButton);
-
-        const yellowButton = document.getElementsByClassName('redChoice')[0];
-       
-        yellowButton.addEventListener('click', () => {
-            game.makeMove('yellow')
-        })
-
-        const blueButton = document.getElementsByClassName('blueChoice')[0];
-
-        blueButton.addEventListener('click', () => {
-            game.makeMove('blue')
-        })
-
-        // chooseButton.addEventListener('click', function(){
-
-        //     game.makeMove();
-        // })
-
-
-        // const colors = ["red", "blue"];
-
-        // const selectColor = document.createElement("select")
-
-    })
-    
 })
 
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+function launchModal() {
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal 
+
+    modal.style.display = "block";
+
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
 }
 
+function initGame() {
+    if (window.__gameOver) {
+        return window.location.reload()
+    }
+    const ctx = canvas.getContext('2d')
+    const numberOfPlayers = prompt('Please enter number between 2 - 8')
+    const game = new Game(ctx, numberOfPlayers);
+    game.drawGame();
+    disabled = document.getElementById('start-button').disabled = true;
 
 
 
-function setUpGameBoard(players = 1) {
 
-    createPlayers(players, 75)
-    createPlayers(players, 185)
+    // const chooseButton = document.createElement('button');
+    // chooseButton.innerText = 'Choose Color';
+
+    document.getElementsByClassName('dropdown')[0].style.visibility = 'visible'
+    // document.getElementById('dropdown').append(chooseButton);
+
+    const yellowButton = document.getElementsByClassName('redChoice')[0];
+
+    yellowButton.addEventListener('click', () => {
+        game.makeMove('red')
+    })
+
+    const blueButton = document.getElementsByClassName('blueChoice')[0];
+
+    blueButton.addEventListener('click', () => {
+        game.makeMove('blue')
+    })
 }
 
-function createPlayer(xCoord = 100, yCoord = 75){ //for a single player
+function createPlayer(xCoord = 100, yCoord = 75) { //for a single player
     // ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.strokeRect(0, 0, canvas.width, canvas.height)
     // ctx.fillRect(20, 200, 100, 100)
-    
+
     ctx.beginPath();
     ctx.arc(xCoord, yCoord, 20, 0, 2 * Math.PI);
     ctx.stroke();
 
-    // requestAnimationFrame(animate)
 }
 
-// function createPlayers(numberOfPlayers, yCoord) { //for multiple players
-//     let startingXCoord = 100
-//     const startingYCoord = yCoord || 75;
-
-//     for (let i = 0; i < numberOfPlayers; i++) {
-//         createPlayer(startingXCoord, startingYCoord);
-//         startingXCoord += 75;
-//     }
-// }
-
-
-
-
-
-
-
-// let player = {
-//     x: 300,
-//     y: 300,
-//     width: 20,
-//     height: 20
-
-//     // x_vel: 0,
-//     // y_vel: 0
-
-// }
-
-
-
-
-
-
-// ctx.strokeRect(100, 100, 100, -100);
-// ctx.fillStyle = 'rgb(200, 0, 0)';
-// ctx.fillRect(10, 10, 50, 50);
-
-// ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-// ctx.fillRect(30, 30, 50, 50);
-
-// function draw() {
-//     var canvas = document.getElementById('canvas');
-//     if (canvas.getContext) {
-//         var ctx = canvas.getContext('2d');
-
-//         ctx.fillRect(25, 25, 100, 100);
-//         ctx.clearRect(45, 45, 60, 60);
-//         ctx.strokeRect(50, 50, 50, 50);
-//     }
-// }
-
-
-//who currently won adn who currenlty lost
-//pass the current winner and the other team.getnextplayer
-//get round winner
+module.exports = {
+    initGame: initGame
+}
